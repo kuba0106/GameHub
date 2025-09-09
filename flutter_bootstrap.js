b2923b4c05013814626e8b0cac3cb40daade2e66
kuba0106 <Jakub.Okragly@gmx.de>
2025-09-10 01:40:44 +0200

    new Version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "950425087"
+    serviceWorkerVersion: "1268192607"
   }
 });
