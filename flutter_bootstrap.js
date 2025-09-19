d5b50088a017df2013a066a19122a0d8f102321c
kuba0106 <Jakub.Okragly@gmx.de>
2025-09-20 01:17:55 +0200

    WordDisplayFix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3318844163"
+    serviceWorkerVersion: "2971335570"
   }
 });
