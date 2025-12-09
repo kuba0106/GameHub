88e6e670c8153d772252dd556b02cd058528ef3b
kuba0106 <Jakub.Okragly@gmx.de>
2025-12-09 15:26:03 +0100

    test hostView Round

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "266241854"
+    serviceWorkerVersion: "1416910572"
   }
 });
